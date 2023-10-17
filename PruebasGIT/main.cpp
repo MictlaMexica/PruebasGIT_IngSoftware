22132d1745f52a91d4e2eafae2282f8bdc224f3f
Angel Ramirez Castro <92240740+AngelRamirez02@users.noreply.github.com>
2023-10-16 18:36:23 -0600

    Add files via upload

---
+++ PruebasGIT/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void FuncionAngel()
+{
+    cout<<"\n\tMensaje de Angel Ramirez Castro para GIT\n";
+}
+
+int main()
+{
+    FuncionAngel();
+    return 0;
+}
