7bd0fe9cc44d9db1e24ef3303d7cc528b719ef38
Juan Pablo Olaza <129029954+MictlaMexica@users.noreply.github.com>
2023-10-16 21:47:30 -0700

    Cambios en main.cpp

--- PruebasGIT/main.cpp
+++ PruebasGIT/main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 void FuncionAngel()
 {
-    cout<<"\n\tMensaje de Angel Ramirez Castro para GIT\nHola\n";
+    cout << "\n\tMensaje de Angel Ramirez Castro para GIT\nHola\n";
+}
+
+void FuncionJuanPa()
+{
+    string nombre;
+    cout << "Por favor, introduce tu nombre: ";
+    cin >> nombre;
+    cout << "Hola, " << nombre << "! Este es tu mensaje personalizado:\n";
 }
 
 int main()
 {
+    FuncionJuanPa();
     FuncionAngel();
     return 0;
 }
